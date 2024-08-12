bc7fdb9482d331f29837815c633a38791caaa124
jtorova5 <jtorova5@gmail.com>
2024-08-12 08:53:32 -0500

    feat: se carga la introducción de .NET

---
+++ AplicacionConsola/Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+string nombre = "Jhonatan";
+string apellido = "Toro";
+int sueldo = 10350000;
+
+Console.Clear();
+Console.WriteLine($"Nombre: {nombre} {apellido}");
+Console.WriteLine($"");
+Console.WriteLine($"Sueldo: {sueldo:C2}");
+Console.WriteLine($"Es decir, ganas {sueldo.ToWords()} pesos Colombianos");
